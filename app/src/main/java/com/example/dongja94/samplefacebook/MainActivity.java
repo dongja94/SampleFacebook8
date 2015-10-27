491c4000b6d63467a44a2b02ce4ac97f8f84d492
dongja94 <dongja94@gmail.com>
2015-10-27 17:46:39 +0900

    Login

--- app/src/main/java/com/example/dongja94/samplefacebook/MainActivity.java
+++ app/src/main/java/com/example/dongja94/samplefacebook/MainActivity.java
@@ -9,18 +9,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
     CallbackManager callbackManager;
+    LoginManager mLoginManager;
+    Button loginButton;
+
+    AccessTokenTracker tracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mLoginManager = LoginManager.getInstance();
         callbackManager = CallbackManager.Factory.create();
         LoginButton button = (LoginButton)findViewById(R.id.btn_auth_login);
         button.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -57,6 +65,61 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        loginButton = (Button)findViewById(R.id.btn_login);
+        setLabel();
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isLogin()) {
+                    mLoginManager.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                        @Override
+                        public void onSuccess(LoginResult loginResult) {
+
+                        }
+
+                        @Override
+                        public void onCancel() {
+
+                        }
+
+                        @Override
+                        public void onError(FacebookException error) {
+
+                        }
+                    });
+                    mLoginManager.logInWithReadPermissions(MainActivity.this, null);
+                } else {
+                    mLoginManager.logOut();
+                }
+            }
+        });
+
+        tracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                setLabel();
+            }
+        };
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        tracker.stopTracking();
+    }
+
+    private void setLabel() {
+        if (!isLogin()) {
+            loginButton.setText("login");
+        } else {
+            loginButton.setText("logout");
+        }
+    }
+
+    private boolean isLogin() {
+        AccessToken token = AccessToken.getCurrentAccessToken();
+        return token==null?false:true;
     }
 
     @Override
