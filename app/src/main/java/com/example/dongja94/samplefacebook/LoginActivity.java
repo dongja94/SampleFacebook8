5016fe7933c443620211f006c04dc1806005308e
dongja94 <dongja94@gmail.com>
2015-10-28 11:16:36 +0900

    auto login

--- app/src/main/java/com/example/dongja94/samplefacebook/LoginActivity.java
+++ app/src/main/java/com/example/dongja94/samplefacebook/LoginActivity.java
@@ -71,4 +71,9 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
