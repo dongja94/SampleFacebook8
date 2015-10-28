24b1a498607e7c3100fceb178d5f0ad937e3c22c
dongja94 <dongja94@gmail.com>
2015-10-28 11:15:37 +0900

    post

--- app/src/main/java/com/example/dongja94/samplefacebook/MyApplication.java
+++ app/src/main/java/com/example/dongja94/samplefacebook/MyApplication.java
@@ -1,6 +1,7 @@
 package com.example.dongja94.samplefacebook;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.facebook.FacebookSdk;
 
@@ -8,9 +9,15 @@ import com.facebook.FacebookSdk;
  * Created by dongja94 on 2015-10-27.
  */
 public class MyApplication extends Application {
+    private static Context mContext;
     @Override
     public void onCreate() {
         super.onCreate();
+        mContext = this;
         FacebookSdk.sdkInitialize(this);
+    }
+
+    public static Context getContext() {
+        return mContext;
     }
 }
